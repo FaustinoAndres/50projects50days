eb38b6558943c4a721d26077050510f6b9f59528
Faustino Correa <49380815+FaustinoAndres@users.noreply.github.com>
2021-03-02 23:15:21 -0300

    Add files via upload

---
+++ Day01/script.js
@@ -0,0 +1,19 @@
+// Image Control
+const cards = document.querySelectorAll('.card');
+
+console.log(cards);
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        removeActiveClass();
+        card.classList.add('active');
+    });
+});
+
+
+function removeActiveClass() {
+    cards.forEach( card => {
+        card.classList.remove('active');
+    });
+}
+
